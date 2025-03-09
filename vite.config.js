ffad01dd533ee922e78725f27a0eedeb99d33b91
yuunik <chorria.zhou@gmail.com>
2025-03-10 03:34:03 +0800

    style(vite): Update import and object formatting

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,43 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
-import UnoCSS from 'unocss/vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import UnoCSS from 'unocss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        // import on demand
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-        // svg icon
-        createSvgIconsPlugin({
-            // Specify the icon folder to be cached
-            iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-            // Specify symbolId format
-            symbolId: 'icon-[dir]-[name]',
-        }),
-        // unocss plugin
-        UnoCSS(),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve('./src'),
-        },
+  plugins: [
+    vue(),
+    // import on demand
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    // svg icon
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // Specify symbolId format
+      symbolId: 'icon-[dir]-[name]',
+    }),
+    // unocss plugin
+    UnoCSS(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'),
     },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                // javascriptEnabled: true,
-                additionalData: '@use "@/styles/variable.scss" as *;',
-            },
-        },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // javascriptEnabled: true,
+        additionalData: '@use "@/styles/variable.scss" as *;',
+      },
     },
-});
+  },
+})
